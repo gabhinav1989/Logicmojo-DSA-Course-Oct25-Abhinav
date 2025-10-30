9919a13cc364cf80cbe22d05e0d0eb47bac87348
gabhinav1989 <gabhinav1989@hotmail.com>
2025-10-29 19:39:56 -0500

    RainWaterTrap

---
+++ RainWaterTrap.java
@@ -0,0 +1,41 @@
+package dsa2025;
+
+public class RainWaterTrap {
+    public static void main(String[] args) {
+        System.out.println(trappedWater(new int[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }));
+    }
+
+    public static int trappedWater(int[] height) {
+        int n = height.length - 1;
+        int left = 0;
+        int right = n;
+        int leftMax = height[left];
+        int rightMax = height[right];
+        int totalWater = 0;
+        // pointers should not cross each other. hence a while loop.
+        while (left <= right) {
+            // increase left if:
+            if (height[left] <= height[right]) { // to determine which side of the wall is of lesser height;
+                // calculate water at that point before moving the left pointer.
+                if (height[left] < leftMax) {
+                    // add water.
+                    totalWater += leftMax - height[left];
+                } else {
+                    leftMax = height[left];
+                }
+                left++;
+
+            } else { // decrease right. but before decreasing, add water.
+                if (height[right] < rightMax) {
+                    // add water.
+                    totalWater += rightMax - height[right];
+                } else {
+                    rightMax = height[right];
+                }
+
+                right--;
+            }
+        }
+        return totalWater;
+    }
+}
